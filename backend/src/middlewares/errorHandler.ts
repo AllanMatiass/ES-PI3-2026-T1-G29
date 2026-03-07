3dc79a9e1f597e6a11aa6b5c829b91f9ed5c6aeb
Allan Matias <alangiovanepaes@gmail.com>
2026-03-07 18:03:35 -0300

    feat: signup

---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,43 @@
+import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/AppError.js";
+import type { ApiErrorResponse, ApiResponse } from "../types/http.js";
+
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  const now = new Date().toISOString();
+  if (err instanceof AppError) {
+    const error = {
+      status: err.statusCode,
+      message: err.message,
+      timestamp: now,
+      path: req.originalUrl
+    } as ApiErrorResponse;
+
+    const body = {
+      success: false,
+      error
+    } as ApiResponse<void>;
+
+    
+
+    return res.status(err.statusCode).json(body);
+  }
+
+  const internalError = {
+    status: 500,
+    message: "An unexpected error occurred",
+    timestamp: now,
+    path: req.originalUrl
+  } as ApiErrorResponse;
+
+
+  return res.status(500).json({
+    success: false,
+    error: internalError
+  } as ApiResponse<void>);
+}
