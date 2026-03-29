285147816be3b9cf6b8d4179af5977b009d0636c
Allan Matias <alangiovanepaes@gmail.com>
2026-03-29 19:10:28 -0300

    refactor: creating mockup functions just for tests

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,32 +1,32 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+const app = admin.initializeApp();
+const db = app.firestore();
+const collectionUsers = db.collection("users");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const mockupFunction = functions.https.onRequest(async (req, res) => {
+  const user = {
+    email: "allangiovannimatias@gmail.com",
+    name: "John Doe",
+    cpf: "12345678900",
+    password: "123456",
+    field: 1,
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+  };
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  try {
+    const createUser = await admin.auth().createUser({
+      email: user.email,
+      password: user.password,
+      displayName: user.name,
+    });
+    await collectionUsers.doc(createUser.uid).set({
+      cpf: user.cpf, anyField: user.field,
+    });
+    res.send(`User created with ID: ${createUser.uid}`);
+  } catch (e) {
+    functions.logger.error("Error creating user:", e);
+    res.status(500).send("Error creating user");
+  }
 });
