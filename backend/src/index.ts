4358a0feda6c7a2af68bd746354ffa40a7adc79c
Allan Matias <alangiovanepaes@gmail.com>
2026-04-12 12:06:11 -0300

    core: creating error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import {config} from 'dotenv';
-import { firebaseTest } from './config/firebase.js';
-import  authRoutes  from './routes/public/authRoutes.js';
-import  userRoutes  from './routes/private/userRoutes.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { authMiddleware } from './middlewares/authMiddleware.js';
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { firebaseTest } from "./config/firebase.js";
+import authRoutes from "./routes/public/authRoutes.js";
+import userRoutes from "./routes/private/userRoutes.js";
+import { errorHandler } from "../../firebase/functions/src/middlewares/errorHandler.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 config();
 await firebaseTest();
@@ -16,14 +16,11 @@ const PORT = process.env.BACKEND_PORT;
 app.use(express.json());
 app.use(cors());
 
-app.use('/auth', authRoutes);
-app.use('/user', authMiddleware, userRoutes);
+app.use("/auth", authRoutes);
+app.use("/user", authMiddleware, userRoutes);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log('Server started');
+  console.log("Server started");
 });
-
-
-
