825b241ef3c17126cf04600c831dcb629ff55724
Allan Matias <alangiovanepaes@gmail.com>
2026-04-23 23:53:42 -0300

    hotfix: authentication in getStartupDetails function was fixed

--- firebase/functions/src/startups/handlers/getStartupDetails.ts
+++ firebase/functions/src/startups/handlers/getStartupDetails.ts
@@ -1,6 +1,5 @@
 // Autor: Allan Giovanni Matias Paes
 import { HttpsError, onCall } from "firebase-functions/v2/https";
-// import { requireAuthenticatedUser } from "../../shared/auth";
 import { normalizeString } from "../../shared/validation";
 import { withCallHandler } from "../../shared/middlewares/errorHandler";
 import {
@@ -9,6 +8,7 @@ import {
   StartupDetails,
 } from "../types/dtos";
 import { InvestmentMetricService } from "../shared/investmentMetricService";
+import { requireAuthenticatedUser } from "../../shared/auth";
 
 const investmentMetricService = new InvestmentMetricService();
 
@@ -27,15 +27,13 @@ const investmentMetricService = new InvestmentMetricService();
 export const getStartupDetails = onCall(
   withCallHandler<GetStartupDetailsRequest, GetStartupDetailsResponse>(
     async (request) => {
-      // const user = requireAuthenticatedUser(request);
+      const user = requireAuthenticatedUser(request);
 
       const startupId = normalizeString(request.data?.id);
 
       if (!startupId) {
         throw new HttpsError("invalid-argument", "Informe o id da startup.");
       }
-
-      const userId = request.auth?.uid ?? "anonymous";
 
       const {
         startup,
@@ -46,7 +44,7 @@ export const getStartupDetails = onCall(
         valuation,
         isInvestor,
         questions,
-      } = await investmentMetricService.getStartupMetrics(startupId, userId);
+      } = await investmentMetricService.getStartupMetrics(startupId, user.uid);
 
       const data: StartupDetails = {
         startup,
