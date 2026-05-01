6ee84beb518c5e266ab12d5f68dd3690f7c5f9cf
Allan Matias <alangiovanepaes@gmail.com>
2026-04-30 21:46:23 -0300

    fix: fixing faq typing

--- firebase/functions/src/startups/types/index.ts
+++ firebase/functions/src/startups/types/index.ts
@@ -1,6 +1,6 @@
 // Autor: Allan Giovanni Matias Paes
 import { Timestamp } from "firebase-admin/firestore";
-import { StartupQuestionCreateInput, Variation } from "./dtos";
+import { StartupQuestionCreateDTO, Variation } from "./dtos";
 
 /**
  * Representa os estágios de maturidade aceitos para uma startup.
@@ -93,7 +93,7 @@ export type StartupDocument = {
   updatedAt?: Timestamp;
 };
 
-export type StartupQuestionDocument = StartupQuestionCreateInput & {
+export type StartupQuestionDocument = StartupQuestionCreateDTO & {
   answers: StartupQuestionAnswer[];
 };
 
