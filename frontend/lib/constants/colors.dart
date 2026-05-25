32cf00476c0899bd5cb9cb1f488ae0b12dd36cd6
Allan Matias <alangiovanepaes@gmail.com>
2026-05-25 16:08:30 -0300

    feat: confirmation modal

--- frontend/lib/constants/colors.dart
+++ frontend/lib/constants/colors.dart
@@ -2,15 +2,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Primary colors
+  // Primary brand colors
   static const Color primary = Color(0xFF00A84E);
   static const Color primaryDark = Color(0xFF00873E);
+  static final Color primaryLight = const Color(0xFF00A84E).withOpacity(0.1);
 
   // Neutral colors
   static const Color surfaceDark = Color(0xFF0F172A);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color transparent = Colors.transparent;
+  
   static final Color grey50 = Colors.grey[50]!;
   static final Color grey100 = Colors.grey[100]!;
   static final Color grey200 = Colors.grey[200]!;
@@ -26,8 +28,22 @@ class AppColors {
   static const Color danger = Color(0xFFEF4444);
   static const Color dangerLight = Color(0xFFFEF2F2);
   static const Color dangerBorder = Color(0xFFFEE2E2);
-  static const Color warning = Colors.amber;
-  static const Color info = Colors.blue;
+  static const Color warning = Color(0xFFF59E0B); // Orange-ish Amber
+  static final Color warningLight = const Color(0xFFF59E0B).withOpacity(0.1);
+  static const Color info = Color(0xFF3B82F6); // Standard Blue
+  static final Color infoLight = const Color(0xFF3B82F6).withOpacity(0.1);
+
+  // Chart Colors (for AssetsPieChart)
+  static const List<Color> chartPalette = [
+    primary,
+    info,
+    warning,
+    Color(0xFF8E24AA), // Purple
+    Color(0xFFF4511E), // Deep Orange
+    Color(0xFF00ACC1), // Cyan
+    Color(0xFFD81B60), // Pink
+    Color(0xFF43A047), // Green
+  ];
 
   // Shimmer colors
   static Color shimmerBaseLight = Colors.grey[300]!;
